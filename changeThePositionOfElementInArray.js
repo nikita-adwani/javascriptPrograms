0b13d0e0c4abe4ac4fd07ad1b0e50ba975cd3c84
nikita-adwani <niknikkynikita@gmail.com>
2020-12-02 16:22:39 +0530

    swapping the position of element in array
    Swapping of first and  last element of array.

---
+++ changeThePositionOfElementInArray.js
@@ -0,0 +1,24 @@
+var readlineSync = require('readline-sync');
+var changePosition = () => {
+    var limitOfArray = readlineSync.question('Enter the limit of array:');
+    console.log("Limit is : " + limitOfArray);
+    var array = [];
+    if (limitOfArray >= 2) {
+        for (i = 0; i < limitOfArray; i++) {
+            var elementsOfArray = readlineSync.question('Enter the elements of array:');
+            array.push(parseInt(elementsOfArray));
+        }
+        console.log(`The array entered is: ${array}`);
+        var swapArray = [];
+        var temp;
+        for (i = 0; i < array.length; i++) {
+            swapArray.push(array[i]);
+        }
+        temp = swapArray[0];
+        swapArray[0] = swapArray[swapArray.length - 1];
+        swapArray[swapArray.length - 1] = temp;
+        console.log(`The resulted array is: ${swapArray}`);
+
+    }
+}
+changePosition();
